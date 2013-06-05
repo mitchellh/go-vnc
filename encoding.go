89ffb026dd3ee80a5c514bfbf2d82b1c3873739f
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-06-05 12:51:15 -0700

    Parse the actual pixels for the raw encoding

--- encoding.go
+++ encoding.go
@@ -21,6 +21,7 @@ type Encoding interface {
 //
 // See RFC 6143 Section 7.7.1
 type RawEncoding struct {
+	Colors []Color
 }
 
 func (*RawEncoding) Type() int32 {
@@ -36,15 +37,32 @@ func (*RawEncoding) Read(c *ClientConn, rect *Rectangle, r io.Reader) (Encoding,
 		byteOrder = binary.BigEndian
 	}
 
+	colors := make([]Color, rect.Height * rect.Width)
 	for y := uint16(0); y < rect.Height; y++ {
 		for x := uint16(0); x < rect.Width; x++ {
-			if err := binary.Read(r, byteOrder, pixelBytes); err != nil {
+			if _, err := io.ReadFull(r, pixelBytes); err != nil {
 				return nil, err
 			}
 
-			// TODO(mitchellh): Do something with the bytes
+			var rawPixel uint32
+			if c.PixelFormat.BPP == 8 {
+				rawPixel = uint32(pixelBytes[0])
+			} else if c.PixelFormat.BPP == 16 {
+				rawPixel = uint32(byteOrder.Uint16(pixelBytes))
+			} else if c.PixelFormat.BPP == 32 {
+				rawPixel = byteOrder.Uint32(pixelBytes)
+			}
+
+			color := &colors[x+y]
+			if c.PixelFormat.TrueColor {
+				color.R = uint16((rawPixel << c.PixelFormat.RedShift) & uint32(c.PixelFormat.RedMax))
+				color.G = uint16((rawPixel << c.PixelFormat.GreenShift) & uint32(c.PixelFormat.GreenMax))
+				color.B = uint16((rawPixel << c.PixelFormat.BlueShift) & uint32(c.PixelFormat.BlueMax))
+			} else {
+				*color = c.ColorMap[rawPixel]
+			}
 		}
 	}
 
-	return &RawEncoding{}, nil
+	return &RawEncoding{colors}, nil
 }
